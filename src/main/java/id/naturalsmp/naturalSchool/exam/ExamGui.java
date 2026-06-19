02674a60465bb778eb9342854303276e2ffde9ac
ImJopeh <jopedev@gmail.com>
2026-06-19 20:01:38 +0700

    fix: resolve IllegalArgumentException in renderPortalUjianJava due to multiple initially selected options

--- src/main/java/id/naturalsmp/naturalSchool/exam/ExamGui.java
+++ src/main/java/id/naturalsmp/naturalSchool/exam/ExamGui.java
@@ -300,7 +300,7 @@ public class ExamGui {
             selectOptions.add(io.papermc.paper.registry.data.dialog.input.SingleOptionDialogInput.OptionEntry.create(
                 String.valueOf(subjectId),
                 Component.text(subjectName),
-                true
+                subjectId == 1
             ));
         }
 
